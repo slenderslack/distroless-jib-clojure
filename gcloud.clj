882711c9a17a849a1e7848339c525aaa0018df4d
Jim Clark <jim@atomist.com>
2021-12-02 15:19:34 -0800

    need to trim stdout for gcloud auth print-access-token

--- gcloud.clj
+++ gcloud.clj
@@ -1,6 +1,7 @@
 (ns gcloud
-  (:require [clojure.java.shell :as sh]))
+  (:require [clojure.java.shell :as sh]
+            [clojure.string :as s]))
 
 ;; needs gcloud auth login to be complete
 (defn authorizer [_]
-  {:username "oauth2accesstoken" :password (:out (sh/sh "gcloud" "auth" "print-access-token"))})
+  {:username "oauth2accesstoken" :password (s/trim (:out (sh/sh "gcloud" "auth" "print-access-token")))})
