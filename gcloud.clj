52942f8fae1ffd9e2de671f651a79b354909b993
Jim Clark <jim@atomist.com>
2021-12-06 19:51:05 -0800

    bump

--- gcloud.clj
+++
@@ -1,7 +0,0 @@
-(ns gcloud
-  (:require [clojure.java.shell :as sh]
-            [clojure.string :as s]))
-
-;; needs gcloud auth login to be complete
-(defn authorizer [_]
-  {:username "oauth2accesstoken" :password (s/trim (:out (sh/sh "gcloud" "auth" "print-access-token")))})
